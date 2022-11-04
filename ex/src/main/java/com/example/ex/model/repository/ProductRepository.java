e0c5fe8c8876172e6e5c7cc253903fc3378beee7
olyao <olya.olka.ena@mail.ru>
2022-11-04 18:46:43 +0300

    shop

---
+++ ex/src/main/java/com/example/ex/model/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.ex.model.repository;
+
+import com.example.ex.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
---
+++ ex/src/main/java/com/example/ex/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.example.ex.service;
+
+import com.example.ex.model.entity.Product;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listProducts(String title);
+    void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3 )  throws IOException;
+    void deleteProduct(Long id);
+    Product getProductById(Long id);
+}
