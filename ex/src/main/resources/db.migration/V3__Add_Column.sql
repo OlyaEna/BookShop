c2748effca795f9b6d407c15628eafd5729116fc
olyao <olya.olka.ena@mail.ru>
2022-12-05 02:31:24 +0300

    shop

---
+++ ex/src/main/resources/db.migration/V3__Add_Column.sql
@@ -0,0 +1,20 @@
+ALTER TABLE order_items add column quantity int;
+ALTER TABLE order_items add column total_price double;
+ALTER TABLE order_items add column total_price double;
+ALTER TABLE order_items add column price double;
+ALTER TABLE order_items add column title varchar(255);
+ALTER TABLE users add column country varchar(255);
+ALTER TABLE users add column city varchar(255);
+ALTER TABLE users add column address varchar(255);
+ALTER TABLE users add column image MEDIUMBLOB;
+
+DELETE FROM users WHERE id=7;
+
+
+
+
+
+
+
+
+
